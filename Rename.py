5c08294bd9f490656f0e155803138e801fb83b08
sbwww <45259695+sbwww@users.noreply.github.com>
2020-11-02 23:34:09 +0800

    Modified student list and main program
    Added optional courses in the student list
    Modified the definition of constants in the main program

--- Rename.py
+++ Rename.py
@@ -2,32 +2,39 @@ import os
 import re
 import sys
 
-from StuList import *
+import StuList
 
-PREFIX = "计科(5)班"
-SUFFIX = "实验六."
+PREFIX = ""
+SUFFIX = "实验一."
+SLIST = StuList.WEB_LIST
 
-filelist = os.listdir("实验6")
+filelist = os.listdir("web实验")
 currentpath = os.getcwd()
-os.chdir(r"实验6")
+os.chdir(r"web实验")
 
 expected = len(SLIST)
 received = len(filelist)
 renamed = 0
 received_list = []
+startPos = len(PREFIX)
+endPos = startPos+12
+
 # 改名
 for filename in filelist:
     split_name = filename.split('-')
     if len(split_name) == 1:
-        sNo = filename[6:18] if filename[6:18].isdigit() else filename[6:17]
+        if filename[startPos:endPos].isdigit():
+            sNo = filename[startPos:endPos]
+        else:
+            SNo = filename[startPos:endPos-1]
         received_list.append(sNo)
         continue
-    if len(split_name) != 3:
+    if len(split_name) != 2:
         print(filename+" \t ERROR!")
         continue
     sNo = split_name[0]  # 学号
-    sName = split_name[1]  # 姓名
-    docType = split_name[2].split('.')[1]  # 文件类型
+    sName = split_name[1].split('.')[0]  # 姓名
+    docType = split_name[1].split('.')[1]  # 文件类型
     os.rename(filename, PREFIX+sNo+sName+SUFFIX+docType)
     received_list.append(sNo)
     renamed += 1

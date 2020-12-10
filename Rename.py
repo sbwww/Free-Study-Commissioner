768a7f19df68eb30920343ee6a80a7d92ba94482
sbwww <45259695+sbwww@users.noreply.github.com>
2020-12-10 16:22:47 +0800

    update

--- Rename.py
+++ Rename.py
@@ -4,13 +4,32 @@ import sys
 
 import StuList
 
-PREFIX = ""
-SUFFIX = "实验一."
-SLIST = StuList.WEB_LIST
+CHN_NUMBER = ['', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十']
 
-filelist = os.listdir("web实验")
-currentpath = os.getcwd()
-os.chdir(r"web实验")
+PREFIX = ""
+SUFFIX = "实验"
+
+input_suffix = input("实验几：")
+if input_suffix.isdigit():
+    SUFFIX += CHN_NUMBER[(int)(input_suffix)]
+else:
+    SUFFIX += input_suffix
+
+SUFFIX += "."
+
+input_course = input("什么课：")
+# 按照课程增加和修改 elif
+if input_course[0] == 'w' or input_course[0] == 'W':
+    SLIST = StuList.WEB_LIST
+    filelist = os.listdir("web实验")
+    currentpath = os.getcwd()
+    os.chdir(r"web实验")
+elif input_course[0] == 'c' or input_course[0] == 'C':
+    SLIST = StuList.CSHARP_LIST
+    filelist = os.listdir("c#实验")
+    currentpath = os.getcwd()
+    os.chdir(r"c#实验")
+
 
 expected = len(SLIST)
 received = len(filelist)
@@ -40,12 +59,14 @@ for filename in filelist:
     renamed += 1
 
 # print(received_list)
-print("Expected: " + str(expected) + "\t files.")
-print("Received: " + str(received) + "\t files.")
-print(" Renamed: " + str(renamed) + "\t files.")
+print("Expected:   " + str(expected) + "\t files.")
+print("Received:   " + str(received) + "\t files.")
+print(" Renamed:   " + str(renamed) + "\t files.")
 for student in SLIST:
     if str(student[0]) not in received_list:
-        print(str(student[0])+" "+str(student[1])+" not received!")
+        print(str(student[0])+"\t"+str(student[1])+"\tnot received!")
 
 os.chdir(currentpath)
 sys.stdin.flush()
+
+os.system("pause")
